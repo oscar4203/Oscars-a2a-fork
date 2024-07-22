d4c9adf2acae39dd0a33fa995d58da6e0b4375af
Kevin Ulliac <kevanoullio@outlook.com>
2024-07-22 16:50:04 -0600

    Added agent_training.py program for training agent model manually.

---
+++ agent_training.py
@@ -0,0 +1,299 @@
+# Description: Main driver for the 'Apples to Apples' game.
+
+# Standard Libraries
+import logging
+import argparse
+
+# Third-party Libraries
+from gensim.models import KeyedVectors
+
+# Local Modules
+from source.config import configure_logging
+from source.apples import GreenApple, RedApple, Deck
+from source.agent import Agent, HumanAgent, RandomAgent, AIAgent, model_type_mapping
+# from source.model import Model, LRModel, NNModel
+from source.results import GameResults, log_results
+from source.w2vloader import VectorsW2V
+
+
+class ApplesToApples:
+    def __init__(self, number_of_rounds: int, green_expansion: str = '', red_expansion: str = '') -> None:
+        self.number_of_rounds: int = number_of_rounds
+        self.green_expansion_filename: str = green_expansion
+        self.red_expansion_filename: str = red_expansion
+        self.green_apples_deck: Deck = Deck()
+        self.red_apples_deck: Deck = Deck()
+        self.winner: Agent | None = None
+        self.human: Agent = HumanAgent("Human Agent")
+        self.agent: Agent | None = None
+        self.round: int = 0
+        self.current_judge: Agent | None = None
+        self.green_apples_in_play: dict[Agent, GreenApple] | None = None
+        self.red_apples_in_play: list[dict[str, RedApple]] = []
+        self.discarded_green_apples: list[GreenApple] = []
+        self.discarded_red_apples: list[RedApple] = []
+        self.nlp_model: KeyedVectors = KeyedVectors.load_word2vec_format("./apples/GoogleNews-vectors-negative300.bin", binary=True)
+        # self.vectors = VectorsW2V("./apples/GoogleNews-vectors-negative300.bin")
+        # embeddings.load()
+
+    def start(self) -> None:
+        print("Starting 'Apples to Apples' agent training app.")
+        logging.info("Starting 'Apples to Apples' agent training app.")
+        logging.info("Initializing agent.")
+
+        # Initialize the decks
+        self.__initialize_decks()
+
+        # Initialize the players
+        self.__initialize_players()
+
+        # Choose the starting judge
+        self.__choose_judge()
+
+        # Start the game loop
+        self.__game_loop()
+
+    def __initialize_decks(self) -> None:
+        # Load the green apples deck
+        self.green_apples_deck.load_deck("Green Apples", "./apples/green_apples.csv")
+        print(f"Loaded {len(self.green_apples_deck.apples)} green apples.")
+        logging.info(f"Loaded {len(self.green_apples_deck.apples)} green apples.")
+
+        # Load the red apples deck
+        self.red_apples_deck.load_deck("Red Apples", "./apples/red_apples.csv")
+        print(f"Loaded {len(self.red_apples_deck.apples)} red apples.")
+        logging.info(f"Loaded {len(self.red_apples_deck.apples)} red apples.")
+
+        # Load the green apples expansion deck
+        if self.green_expansion_filename:
+            self.green_apples_deck.load_deck("Green Apples Expansion", self.green_expansion_filename)
+            print(f"Loaded {len(self.green_apples_deck.apples)} green apples from the expansion.")
+            logging.info(f"Loaded {len(self.green_apples_deck.apples)} green apples from the expansion.")
+
+        # Load the red apples expansion deck
+        if self.red_expansion_filename:
+            self.red_apples_deck.load_deck("Red Apples Expansion", self.red_expansion_filename)
+            print(f"Loaded {len(self.red_apples_deck.apples)} red apples from the expansion.")
+            logging.info(f"Loaded {len(self.red_apples_deck.apples)} red apples from the expansion.")
+
+        # Shuffle the decks
+        self.green_apples_deck.shuffle()
+        self.red_apples_deck.shuffle()
+
+    def __initialize_players(self) -> None:
+        # Validate the user input for the model type
+        model_type: str = ""
+        model_type = input("Please enter the model type (1: Linear Regression, 2: Neural Network): ")
+        logging.info(f"Please enter the model type (1: Linear Regression, 2: Neural Network): {model_type}")
+        while model_type not in ['1', '2']:
+            model_type = input("Invalid input. Please enter the model type (1: Linear Regression, 2: Neural Network): ")
+            logging.error(f"Invalid input. Please enter the model type (1: Linear Regression, 2: Neural Network): {model_type}")
+
+        # Generate a unique name for the AI agent
+        model_type_class = model_type_mapping[model_type]
+        logging.debug(f"Model Type Class: {model_type_class}")
+        logging.debug(f"Model Type Name: {model_type_class.__name__}")
+        new_agent_name = f"AI Agent - {model_type_class.__name__}"
+        new_agent = AIAgent(new_agent_name, model_type_class)
+
+        # Append the player object
+        self.agent = new_agent
+        logging.info(self.agent)
+
+        # Have each player pick up 7 red cards
+        self.agent.draw_red_apples(self.red_apples_deck)
+
+        # Add the human player to a list
+        human_list = [self.human]
+
+        # Initialize the models for the AI agents
+        if isinstance(self.agent, AIAgent):
+            self.agent.initialize_models(self.nlp_model, human_list)
+            logging.info(f"Initialized models for {new_agent.name}.")
+
+    def __choose_judge(self) -> None:
+        # Set the judge
+        self.current_judge = self.human
+        self.human.judge = True
+
+        # Check that the agent is not None
+        if self.agent is None:
+            logging.error("The agent is None.")
+            raise ValueError("The agent is None.")
+
+        self.agent.judge = True
+        print(f"{self.agent.name} is the starting judge.")
+
+    # def __assign_next_judge(self) -> None:
+    #     # Assign the next judge
+    #     self.human.judge = True
+    #     self.current_judge = self.human
+
+    def __is_game_over(self) -> Agent | None:
+        if self.round >= self.number_of_rounds:
+            return self.agent
+        return None
+
+    def __judge_prompt(self) -> None:
+        # Check if the current judge is None
+        if self.current_judge is None:
+            logging.error("The current judge is None.")
+            raise ValueError("The current judge is None.")
+
+        # Prompt the judge to draw a green card
+        print(f"\n{self.current_judge.name}, please draw a green card.")
+        logging.info(f"{self.current_judge.name}, please draw a green card.")
+
+        # Set the green card in play
+        self.green_apples_in_play = {self.human: self.current_judge.draw_green_apple(self.green_apples_deck)}
+
+    def __player_prompt(self) -> None:
+        # Check if the agent is None
+        if self.agent is None:
+            logging.error("The agent is None.")
+            raise ValueError("The agent is None.")
+
+        print(f"\n{self.agent.name}, please select a red card.")
+        logging.info(f"{self.agent.name}, please select a red card.")
+
+        # Check if the current judge is None
+        if self.current_judge is None:
+            logging.error("The current judge is None.")
+            raise ValueError("The current judge is None.")
+
+        # Check if the green apples in play is None
+        if self.green_apples_in_play is None:
+            logging.error("The green apples in play is None.")
+            raise ValueError("The green apples in play is None.")
+
+        # Set the red cards in play
+        red_apple = self.agent.choose_red_apple(self.current_judge, self.green_apples_in_play[self.current_judge])
+        self.red_apples_in_play.append({self.agent.name: red_apple})
+        logging.info(f"Red card: {red_apple}")
+
+        # Prompt the player to pick up a new red card
+        if len(self.agent.red_apples) < 7:
+            self.agent.draw_red_apples(self.red_apples_deck)
+
+    def __game_loop(self) -> None:
+        # Start the game loop
+        while self.winner is None:
+            # Increment the round
+            self.round += 1
+
+            # Print and log the round message
+            round_message = f"\n===================" \
+                            f"\nROUND {self.round}:" \
+                            f"\n===================\n"
+            print(round_message)
+            logging.info(round_message)
+
+            # # Assign the human as judge
+            # self.__assign_next_judge()
+
+            # Prompt the judge to select a green card
+            self.__judge_prompt()
+
+            # Prompt the players to select a red card
+            self.__player_prompt()
+
+            # Check if the current judge is None
+            if self.current_judge is None:
+                logging.error("The current judge is None.")
+                raise ValueError("The current judge is None.")
+
+            # Check if the green apples in play is None
+            if self.green_apples_in_play is None:
+                logging.error("The green apples in play is None.")
+                raise ValueError("The green apples in play is None.")
+
+            # Prompt the judge to select the winning red card
+            print(f"\n{self.current_judge.name}, please select the winning red card.")
+            logging.info(f"{self.current_judge.name}, please select the winning red card.")
+            winning_red_card_dict: dict[str, RedApple] = self.current_judge.choose_winning_red_apple(
+                self.green_apples_in_play[self.current_judge], self.red_apples_in_play)
+
+            # Check for None values
+            if self.agent is None:
+                logging.error("The agent is None.")
+                raise ValueError("The agent is None.")
+
+            if self.green_apples_in_play is None:
+                logging.error("The green apples in play is None.")
+                raise ValueError("The green apples in play is None.")
+
+            if self.red_apples_in_play is None:
+                logging.error("The red apples in play is None.")
+                raise ValueError("The red apples in play is None.")
+            else:
+                red_apples_list = []
+                for red_apple in self.red_apples_in_play:
+                    red_apples_list.append(list(red_apple.values())[0])
+
+            # Extract the winning red card
+            winning_red_card: RedApple = list(winning_red_card_dict.values())[0]
+
+            # Put the agent into a list
+            agent_list = [self.agent]
+
+            # Log the results
+            results = GameResults(agent_list, self.number_of_rounds, self.round, self.green_apples_in_play[self.current_judge],
+                                  red_apples_list, winning_red_card, self.current_judge)
+            log_results(results)
+
+            # Train AI agent on human selected apples
+            if isinstance(self.agent, AIAgent):
+                self.agent.train_models(self.nlp_model, self.green_apples_in_play[self.current_judge], winning_red_card, self.current_judge)
+
+            # Discard the green cards
+            self.discarded_green_apples.append(self.green_apples_in_play[self.current_judge])
+            self.green_apples_in_play = None
+
+            # Discard the red cards
+            self.discarded_red_apples.extend(red_apples_list)
+            self.red_apples_in_play = []
+
+             # Check if the game is over
+            self.winner = self.__is_game_over()
+            if self.winner is not None:
+                # Prepare the winner message
+                winner_text = f"# {self.winner.name} has won the game! #"
+                border = '#' * len(winner_text)
+                message = f"\n{border}\n{winner_text}\n{border}\n"
+
+                # Print and log the winner message
+                print(message)
+                logging.info(message)
+
+                break
+
+
+def main() -> None:
+    # Define the command line arguments
+    parser = argparse.ArgumentParser(description="Apples to Apples game configuration.")
+    parser.add_argument("rounds", type=int, choices=range(1, 100), help="Total number of rounds (1-100).")
+    parser.add_argument("green_expansion", type=str, nargs='?', default='', help="Filename to a green card expansion (optional).")
+    parser.add_argument("red_expansion", type=str, nargs='?', default='', help="Filename to a red card expansion (optional).")
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Configure and initialize the logging module
+    configure_logging()
+    logging.info("Starting 'Apples to Apples' agent training application.")
+
+    # Log the command line arguments
+    logging.info(f"Command line arguments: {args}")
+    logging.info(f"Number of rounds: {args.rounds}")
+    logging.info(f"Green card expansion file: {args.green_expansion}")
+    logging.info(f"Red card expansion file: {args.red_expansion}")
+
+    # Create the game object
+    game = ApplesToApples(args.rounds, args.green_expansion, args.red_expansion)
+
+    # Start the game
+    game.start()
+
+
+if __name__ == "__main__":
+    main()
